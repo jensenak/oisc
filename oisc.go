f30f8ed865e0038e9158f128a8eb0d5ec8aee371
Adam Jensen <jensen.ak@gmail.com>
2015-07-22 09:42:07 -0600

    Increased memory capacity to 16384

--- oisc.go
+++ oisc.go
@@ -6,37 +6,57 @@ import (
 )
 
 const (
-	ws   = 3  //wordsize = 3 bytes
-	mmax = 60 // highest allowable ip
+	ws   = 3     //wordsize = 3 bytes
+	mmax = 16381 // highest allowable ip
+	//RESERVED ADDRS
+	zero     = 0
+	minusOne = 1
+	one      = 2
+	count    = 3
+	speed    = 4
+	result   = 5
 )
 
-var mem [64]int8 // memory
-var ip int8      // instruction pointer
-var count int8
+var (
+	mem [mmax + ws - 1]int16 // memory
+	ip  int16                // instruction pointer
+)
 
 func initMem() {
-	mem = [64]int8{0, -1, 0,
-		3, 5, 0, // Things you want to multiply go in these :)
+	a := int16(6) // Multiply value in this address...
+	b := int16(7) // By value here
+	ma := int16(9)
+	mb := int16(10)
+	mem = [mmax + ws - 1]int16{0, 0, 0, // Reserved addrs
+		0, 0, 0, // Reserved addrs
+		9, 5, 0, // Things you want to multiply go in these :)
 		0, 0, 0,
-		3, 6, 0,
-		4, 7, 0,
-		6, 7, 30,
-		6, 6, 0,
-		4, 6, 0,
-		7, 7, 0,
-		3, 7, 36,
-		7, 7, 0,
-		4, 7, 0,
-		1, 6, 0,
-		7, 2, 0,
-		1, 6, 39,
-		0, 0, 63}
+		a, ma, 0, //12
+		b, mb, 0,
+		ma, mb, 33, //18
+		ma, ma, 0,
+		b, ma, 0, //24
+		mb, mb, 0,
+		a, mb, 39, //30
+		mb, mb, 0,
+		b, mb, 0, //36
+		minusOne, ma, 0,
+		mb, result, 0, //42
+		minusOne, ma, 42,
+		0, 0, mmax}
 
-	ip = 9
+	mem[zero] = 0
+	mem[minusOne] = -1
+	mem[one] = 1
+	mem[count] = 0
+	mem[speed] = 500
+	mem[result] = 0
+
+	ip = 12
 }
 
-func subleq(a, b, c int8) int8 {
-	count++
+func subleq(a, b, c int16) int16 {
+	mem[count]++
 	mem[b] = mem[b] - mem[a]  // Perform subtraction
 	if mem[b] <= 0 && c > 0 { // If condition triggered and c availble, jump
 		ip = c
@@ -49,11 +69,11 @@ func subleq(a, b, c int8) int8 {
 	return ip
 }
 
-func terminal(start, end, cols int8) {
+func terminal(start, end, cols int16) {
 	fmt.Print("\033[2J") // Clear screen
 	line := 2
-	col := int8(0)
-	space := int8(12)
+	col := int16(0)
+	space := int16(20)
 	for i := start; i <= end; i++ {
 		fmt.Printf("\033[%d;%dH", line, col)
 		if i >= ip && i <= ip+2 {
@@ -61,6 +81,12 @@ func terminal(start, end, cols int8) {
 		}
 		if i == mem[ip+1] {
 			fmt.Print("\033[1;34m")
+		}
+		if i == result {
+			fmt.Print("\033[1;35m")
+		}
+		if i == count {
+			fmt.Print("\033[1;33m")
 		}
 		fmt.Printf("0x%d:%d", i, mem[i])
 		fmt.Print("\033[0m")
@@ -73,7 +99,7 @@ func terminal(start, end, cols int8) {
 }
 
 func clock() {
-	time.Sleep(time.Second / 2)
+	time.Sleep(time.Millisecond * time.Duration(mem[speed]))
 }
 
 func main() {
